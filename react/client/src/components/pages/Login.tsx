163ecf39827f44df53c98aa28762d93a3e4749a7
Mateusz Zientek <mateusz-z1703@o2.pl>
2023-06-06 23:46:01 +0200

    nowe pola logowania, brack nicku tylko imie i nazwisko

--- react/client/src/components/pages/Login.tsx
+++ react/client/src/components/pages/Login.tsx
@@ -13,9 +13,15 @@ import axios from 'axios';
 import validator from 'validator';
 import tick from "../../assets/images/tick.png"
 
-interface Errors {
-    email: string;
+
+interface Error {
+    msg: string;
+    type: string;
+    value: string;
+    path: string;
+    location: string;
 }
+
 
 function Login() {
     const { theme, setTheme } = useContext(ThemeContext);
@@ -23,23 +29,32 @@ function Login() {
     const [showPassword, setShowPassword] = useState(false);
     const [emailOffert, setEmailOffert] = useState(false);
     const [isUserSaved, setIsUserSaved] = useState(false);
-    const [errors, setErrors] = useState({ email: '', nick: '', pass: '' });
+    const [errors, setErrors] = useState({ email: '', pass: '', name: '', surname: '' });
+    const [errorsVadlidationServer, setErrorsVadlidationServer] = useState<Error[]>([]);
+    const [errorsServer, setErrorsServer] = useState("");
 
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
     const [email, setEmail] = useState('');
-    const [nick, setNick] = useState('');
     const [password, setPassword] = useState('');
 
     const handleEmailOffertChange = () => {
         setEmailOffert(!emailOffert); // Odwróć wartość stanu przy każdej zmianie
     };
 
-    const handleNickChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setNick(event.target.value);
-
-    };
 
     const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPassword(event.target.value);
+
+    };
+
+    const handleSurnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSurname(event.target.value);
+
+    };
+
+    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
 
     };
 
@@ -51,16 +66,23 @@ function Login() {
     const validateData = () => {
         const newErrors = {
             email: '',
-            nick: '',
-            pass: ''
+            pass: '',
+            name: '',
+            surname: ''
         };
 
         if (email && !validator.isEmail(email)) {
             newErrors.email = t('loginError.email');
         }
-        if (nick && /[^a-zA-Z0-9\.\_]/.test(nick)) {
-            newErrors.nick = t('loginError.nick');
+
+        if (name && !/^[a-zA-Z]+$/.test(name)) {
+            newErrors.name = t('loginError.name');
         }
+
+        if (surname && !/^[a-zA-Z]+$/.test(surname)) {
+            newErrors.surname = t('loginError.surname');
+        }
+
         if (password && !validator.isStrongPassword(password, { minSymbols: 0 })) {
             newErrors.pass = t('loginError.pass');
         }
@@ -69,7 +91,7 @@ function Login() {
         setErrors(newErrors);
 
         // Zwróć true, jeśli nie ma żadnych błędów walidacji, w przeciwnym razie false
-        return Object.keys(newErrors.email || newErrors.nick || newErrors.pass).length === 0;
+        return Object.keys(newErrors.email || newErrors.name || newErrors.surname || newErrors.pass).length === 0;
     };
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -78,19 +100,22 @@ function Login() {
         // Waliduj dane przed wysłaniem formularza
         const isValid = validateData();
 
-        if (!email || !password || !nick) {
+
+        if (!email || !password || !name || !surname) {
             // Przynajmniej jedno z pól jest puste
             setErrors({
                 email: !email ? t('loginError.emailReq') : '',
-                nick: !nick ? t('loginError.nickReq') : '',
                 pass: !password ? t('loginError.passReq') : '',
+                name: !name ? t('loginError.nameReq') : '',
+                surname: !surname ? t('loginError.surnameReq') : '',
             });
             return; // Zwracamy funkcję, jeśli którekolwiek pole jest puste
         }
 
         if (isValid) {
             const userData = {
-                nick: nick,
+                name: name,
+                surname: surname,
                 password: password,
                 email: email,
                 role: 'user',
@@ -100,14 +125,26 @@ function Login() {
             axios
                 .post('/login', userData)
                 .then((response) => {
+                    setErrorsServer("");
+                    setErrorsVadlidationServer([])
                     setIsUserSaved(true);
                     setTimeout(() => {
                         navigate(-1);
                     }, 2000);
                 })
                 .catch((error) => {
-                    console.error(error.response.data);
-                    // Dodaj logikę obsługi błędu logowania
+
+                    if (error.response && error.response.data && error.response.data.error) {
+                        setErrorsServer(error.response.data.error)
+
+                    } else if (error.response && error.response.data && error.response.data.errors) {
+                        setErrorsVadlidationServer(error.response.data.errors)
+
+                    } else {
+                        console.log(error);
+                    }
+
+
                 });
         }
 
@@ -124,15 +161,17 @@ function Login() {
     const handleLoginClick = () => {
         const newErrors = {
             email: '',
-            nick: '',
-            pass: ''
+            pass: '',
+            name: '',
+            surname: ''
         };
 
         setErrors(newErrors);
 
-        setNick("");
+        setSurname("");
         setPassword("");
         setEmail("");
+        setName("")
 
         setLoginSelected(true);
     };
@@ -140,15 +179,17 @@ function Login() {
     const handleRegisterClick = () => {
         const newErrors = {
             email: '',
-            nick: '',
-            pass: ''
+            pass: '',
+            name: '',
+            surname: ''
         };
 
         setErrors(newErrors);
 
-        setNick("");
+        setSurname("");
         setPassword("");
         setEmail("");
+        setName("")
 
         setLoginSelected(false);
     };
@@ -158,6 +199,9 @@ function Login() {
 
     return (
         <>
+
+
+
             {isUserSaved && (
                 <div className="bg-black/80 fixed w-full h-screen z-10 flex justify-center items-center ">
                     <div className="flex flex-col  items-center bg-white dark:bg-black dark:border-white dark:border-2 w-[25rem] h-[27rem] lg:w-[35rem] lg:h-[28rem] xl:w-[45rem] xl:h-[30rem] rounded-2xl">
@@ -173,28 +217,31 @@ function Login() {
                 </div>
             )}
 
-            <BsArrowLeft onClick={() => navigate(-1)} className="absolute top-2 lg:top-10 left-10 cursor-pointer hover:scale-110 transition ease-in-out duration-300 " size={50} color={theme === 'dark' ? 'white' : 'black'} />
 
             <div className="bg-[#ececec] dark:bg-black/80 w-screen min-h-screen flex items-center justify-center ">
-                <div className="flex flex-col lg:flex-row my-20 w-[90%] lg:w-[60%] h-screen/2 shadow-button rounded-3xl animate-fade-in-long">
+                <div className="flex flex-col lg:flex-row my-20 w-[90%] lg:w-[80%] 2xl:w-[60%] h-screen/2 shadow-button rounded-3xl animate-fade-in-long">
                     <div className="relative flex justify-center w-full h-[15rem] lg:h-auto lg:w-[50%] ">
                         <img alt="Photo of ocean" className="object-cover w-full h-full rounded-t-3xl lg:rounded-r-none lg:rounded-l-3xl " src={zdjecie} />
                     </div>
 
-                    <div className="flex flex-col items-center bg-white dark:bg-black/60 w-full lg:w-[50%] rounded-b-3xl lg:rounded-l-none lg:rounded-r-3xl">
+
+
+                    <div className="flex flex-col items-center bg-white dark:bg-black/60 w-full lg:w-[50%]  xl:w-[50%] rounded-b-3xl lg:rounded-l-none lg:rounded-r-3xl">
+
+
                         <p className="text-black dark:text-white text-3xl xl:text-4xl pt-10">
                             {isLoginSelected ? t('login.textlogin') : t('login.textregister')}
                         </p>
 
-                        <div className="flex mt-10 text-2xl font-lato mb-10">
+                        <div className="flex mt-10 text-2xl font-lato mb-2">
                             <button
-                                className={`px-8 py-4 border-b-2  hover:text-[#72cff7] ${isLoginSelected ? " border-[#72cff7]  text-[#72cff7]" : "border-black/30 dark:border-white text-black/70 dark:text-white"}`}
+                                className={`px-8 py-4 border-b-2  dark:hover:text-[#72cff7] hover:text-[#72cff7] ${isLoginSelected ? " border-[#72cff7]  text-[#72cff7]" : "border-black/30 dark:border-white text-black/70 dark:text-white"}`}
                                 onClick={handleLoginClick}
                             >
                                 {t('login.signin')}
                             </button>
                             <button
-                                className={`px-8 py-4 border-b-2  hover:text-[#72cff7] ${isLoginSelected ? "border-black/30 dark:border-white text-black/70 dark:text-white" : "border-[#72cff7]  text-[#72cff7]"}`}
+                                className={`px-8 py-4 border-b-2  dark:hover:text-[#72cff7] hover:text-[#72cff7] ${isLoginSelected ? "border-black/30 dark:border-white text-black/70 dark:text-white" : "border-[#72cff7]  text-[#72cff7]"}`}
 
                                 onClick={handleRegisterClick}
                             >
@@ -205,17 +252,32 @@ function Login() {
                         <form className="w-[70%]" onSubmit={handleSubmit}>
                             {!isLoginSelected && (
                                 <input
-                                    value={nick}
+                                    value={name}
                                     onBlur={handleBlur}
-                                    onChange={handleNickChange}
-                                    className={`w-[100%] h-[3rem] bg-[#e9e9e9] px-4 rounded-3xl focus:outline-none focus:border-2 border-black/60 ${errors.nick && "border-2 border-red-400"}`}
-                                    placeholder={t('login.nick') as string}
+                                    onChange={handleNameChange}
+                                    className={`mt-6 w-[100%] h-[3rem] bg-[#e9e9e9] px-4 rounded-3xl focus:outline-none focus:border-2 border-black/60 ${errors.name && "border-2 border-red-400"}`}
+                                    placeholder={t('login.name') as string}
                                 />
 
                             )}
 
                             {!isLoginSelected && (
-                                errors.nick && <p className="text-red-500 text-sm mt-2 ml-2">{errors.nick}</p>
+                                errors.name && <p className="text-red-500 text-sm mt-2 ml-2">{errors.name}</p>
+                            )}
+
+                            {!isLoginSelected && (
+                                <input
+                                    value={surname}
+                                    onBlur={handleBlur}
+                                    onChange={handleSurnameChange}
+                                    className={`mt-6 w-[100%] h-[3rem] bg-[#e9e9e9] px-4 rounded-3xl focus:outline-none focus:border-2 border-black/60 ${errors.surname && "border-2 border-red-400"}`}
+                                    placeholder={t('login.surname') as string}
+                                />
+
+                            )}
+
+                            {!isLoginSelected && (
+                                errors.surname && <p className="text-red-500 text-sm mt-2 ml-2">{errors.surname}</p>
                             )}
 
                             <input
@@ -252,6 +314,12 @@ function Login() {
                             {errors.pass && <p className="text-red-500 text-sm mt-2 ml-2">{errors.pass}</p>}
 
 
+                            {errorsServer && <p className="text-red-500 text-sm mt-2 ml-2">{errorsServer}</p>}
+
+                            {errorsVadlidationServer.map((error, index) => (
+                                <p key={index} className="text-red-500 text-sm mt-2 ml-2">{error.msg}</p>
+                            ))}
+
                             {isLoginSelected && (
                                 <button
                                     type="submit"
@@ -284,7 +352,7 @@ function Login() {
                         )}
 
                         {isLoginSelected && (
-                            <button className="text-black dark:text-white font-lato text-xl mt-6">{t('login.forgot')}</button>
+                            <button className="text-black dark:text-white font-lato text-xl mt-6 hover:scale-105 transition ease-in-out duration-300">{t('login.forgot')}</button>
                         )}
 
                         <button className='flex items-center justify-center space-x-2 mt-10 mb-10 w-[70%] h-[3rem] bg-white border-black border-2  px-4 rounded-3xl hover:scale-105 transition ease-in-out duration-300'>
--- react/server/server.js
+++ react/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { body, validationResult } = require("express-validator");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
@@ -24,7 +25,8 @@ mongoose
   });
 
 const userSchema = new mongoose.Schema({
-  nick: { type: String, required: true },
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, required: true },
@@ -33,51 +35,88 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-app.post("/login", (req, res) => {
-  const { nick, password, email, role, email_offert } = req.body;
+app.post(
+  "/login",
+  [
+    body("email")
+      .notEmpty()
+      .withMessage("Email jest wymagany")
+      .isEmail()
+      .withMessage("Nieprawidłowy format adresu email"),
+    body("password")
+      .notEmpty()
+      .withMessage("Hasło jest wymagane")
+      .isLength({ min: 8 })
+      .withMessage("Hasło musi mieć co najmniej 8 znaków")
+      .matches(/^(?=.*[A-Z])(?=.*\d)/)
+      .withMessage("Hasło musi zawierać przynajmniej 1 dużą literę i cyfrę"),
+    body("name")
+      .notEmpty()
+      .withMessage("Imię jest wymagane")
+      .isAlpha()
+      .withMessage("Imię może zawierać tylko litery"),
+    body("surname")
+      .notEmpty()
+      .withMessage("Nazwisko jest wymagane")
+      .isAlpha()
+      .withMessage("Nazwisko może zawierać tylko litery"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  // Haszowanie hasła
-  bcrypt
-    .hash(password, 10)
-    .then((hashedPassword) => {
-      const user = new User({
-        nick: nick,
-        password: hashedPassword,
-        email: email,
-        role: role,
-        email_offert: email_offert,
-      });
+    const { name, surname, password, email, role, email_offert } = req.body;
 
-      User.findOne({ email: user.email })
-        .then((existingUser) => {
-          if (existingUser) {
-            console.log("Użytkownik już istnieje w bazie danych");
-          } else {
-            user
-              .save()
-              .then(() => {
-                console.log("Użytkownik został zapisany w bazie danych");
-                res
-                  .status(200)
-                  .json({ message: "Użytkownik został zarejestrowany" });
-              })
-              .catch((error) => {
-                console.error("Błąd podczas zapisywania użytkownika:", error);
-                res
-                  .status(500)
-                  .json({ error: "Błąd podczas zapisywania użytkownika" });
-              });
-          }
-        })
-        .catch((error) => {
-          console.error("Błąd podczas wyszukiwania użytkownika:", error);
+    // Haszowanie hasła
+    bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        const user = new User({
+          name: name,
+          surname: surname,
+          password: hashedPassword,
+          email: email,
+          role: role,
+          email_offert: email_offert,
         });
-    })
-    .catch((error) => {
-      console.error("Błąd podczas haszowania hasła:", error);
-      res.status(500).json({ error: "Błąd podczas rejestracji użytkownika" });
-    });
-});
+
+        User.findOne({ email: email })
+          .then((existingUser) => {
+            if (existingUser) {
+              console.log("Podany email jest już przypisany do innego konta");
+              return res.status(400).json({
+                error: "Podany email jest już przypisany do innego konta",
+              });
+            } else {
+              // Kontynuuj zapisywanie użytkownika
+              user
+                .save()
+                .then(() => {
+                  console.log("Użytkownik został zapisany w bazie danych");
+                  res
+                    .status(200)
+                    .json({ message: "Użytkownik został zarejestrowany" });
+                })
+                .catch((error) => {
+                  console.error("Błąd podczas zapisywania użytkownika:", error);
+                  res
+                    .status(500)
+                    .json({ error: "Błąd podczas zapisywania użytkownika" });
+                });
+            }
+          })
+          .catch((error) => {
+            console.error("Błąd podczas wyszukiwania użytkownika:", error);
+          });
+      })
+      .catch((error) => {
+        console.error("Błąd podczas haszowania hasła:", error);
+        res.status(500).json({ error: "Błąd podczas rejestracji użytkownika" });
+      });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
